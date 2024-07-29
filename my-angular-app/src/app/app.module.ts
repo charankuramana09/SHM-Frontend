13f217cde0069368d0b1cea19c5ffeffe8664dcd
rohith-isign <rohith.mamidala@isigntech.com>
2024-07-29 12:11:05 +0530

    Updates_all-Required_tools

--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CompanyInvoiceGenerationComponent } from './company-invoice-generation/company-invoice-generation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyInvoiceGenerationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ my-angular-app/src/app/company-invoice-generation/company-invoice-generation.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+import { jsPDF } from 'jspdf';
+import autoTable, { RowInput } from 'jspdf-autotable';
+
+
+
+@Component({
+  selector: 'app-company-invoice-generation',
+  templateUrl: './company-invoice-generation.component.html',
+  styleUrl: './company-invoice-generation.component.scss'
+})
+export class CompanyInvoiceGenerationComponent {
+
+  
+}
