4bf84f57c8ec52d055da3e707ec1cb8f2c37a8b8
Mallikarjun-iSign <mallikarjuna.gone@isigntech.com>
2024-07-31 14:09:25 +0530

    update-p

--- my-angular-app/src/app/auth/auth.service.ts
+++ my-angular-app/src/app/auth/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) { }
 
   login(loginData: { email: string, password: string }): Observable<any> {
-    return this.http.post<{ token: string, authorities: string[], firstName: string, lastName: string }>(${this.apiUrl}/login, loginData).pipe(
+    return this.http.post<{ token: string, authorities: string[], firstName: string, lastName: string }>(`${this.apiUrl}/login`, loginData).pipe(
       tap(response => {
         localStorage.setItem('jwtToken', response.token);
         localStorage.setItem('authorities', JSON.stringify(response.authorities));
