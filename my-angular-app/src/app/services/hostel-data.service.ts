775e12f6659ba3bb9e9444f5f2b65d914622f5d4
rohith-isign <rohith.mamidala@isigntech.com>
2024-08-02 20:30:18 +0530

    admin dashboard done

--- my-angular-app/src/app/services/hostel-data.service.ts
+++ my-angular-app/src/app/services/hostel-data.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,18 @@ export class HostelDataService {
 
   getHostelName() {
     return this.hostelName;
+  }
+
+
+  private apiUrl = 'http://localhost:8081/admin/filter'; // Update with your actual backend URL
+
+  constructor(private http: HttpClient) {}
+
+  getUserDetailsByFrequencyType(frequencyType: string, hostelName: string): Observable<Object[]> {
+    let params = new HttpParams();
+    params = params.append('frequencyType', frequencyType);
+    params = params.append('hostelName', hostelName);
+
+    return this.http.get<Object[]>(this.apiUrl, { params });
   }
 }
