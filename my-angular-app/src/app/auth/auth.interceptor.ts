f0b2826a2aa013e6973ac54e6e7a0c07f84316f5
rohith-isign <rohith.mamidala@isigntech.com>
2024-08-02 14:52:19 +0530

    updates

--- my-angular-app/src/app/auth/auth.interceptor.ts
+++ my-angular-app/src/app/auth/auth.interceptor.ts
@@ -1,25 +1,54 @@
 // In auth.interceptor.ts
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
   private excludedEndpoints = ['/register', '/login'];
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (this.excludedEndpoints.some(endpoint => req.url.includes(endpoint))) {
       return next.handle(req);
     }
-    const token = localStorage.getItem('jwtToken');
 
-    if (token) {
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${token}`)
-      });
+    if (this.isBrowser) {
+      const token = localStorage.getItem('jwtToken');
 
-      return next.handle(cloned);
-    } else {
-      return next.handle(req);
+      if (token) {
+        const cloned = req.clone({
+          headers: req.headers.set('Authorization', `Bearer ${token}`)
+        });
+
+        return next.handle(cloned);
+      }
     }
+
+    return next.handle(req);
   }
+
+  // private excludedEndpoints = ['/register', '/login'];
+  // intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  //   if (this.excludedEndpoints.some(endpoint => req.url.includes(endpoint))) {
+  //     return next.handle(req);
+  //   }
+  //   const token = localStorage.getItem('jwtToken');
+
+  //   if (token) {
+  //     const cloned = req.clone({
+  //       headers: req.headers.set('Authorization', `Bearer ${token}`)
+  //     });
+
+  //     return next.handle(cloned);
+  //   } else {
+  //     return next.handle(req);
+  //   }
+  // }
 }
