0178c8de372e766ebe6bbe6c299f1b0473f5e31f
sivakumar-isign <sivakumar.kamuju@isigntech.com>
2024-08-04 23:27:41 +0530

    update

--- my-angular-app/src/app/services/admin.service.ts
+++ my-angular-app/src/app/services/admin.service.ts
@@ -22,4 +22,15 @@ export class AdminService {
   getAdminDetails(): Observable<AdminData[]> {
     return this.http.get<AdminData[]>(this.apiUrl);
   }
+  sendInvoice(pdfOutput: Blob, email: string, companyName: string): Observable<string> {
+    const formData = new FormData();
+    formData.append('file', pdfOutput, 'invoice.pdf');
+    formData.append('email', email);
+    formData.append('name', companyName);
+
+    return this.http.post('http://localhost:8081/email/sendInvoice', formData, { responseType: 'text' });
+  }
+  validateMobileNumbers(mobileNumbers: number[]): Observable<any> {
+    return this.http.post<any>('http://localhost:8081/admin/validate-mobile-numbers', mobileNumbers);
+  }
 }
